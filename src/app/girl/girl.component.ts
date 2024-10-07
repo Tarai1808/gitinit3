0b5a54bce00b88793fa848e0bd537eb0fea9dfab
Tarai1808 <ashiruzakariyyaaidris6767@gmail.com>
2024-10-07 13:44:46 +0100

    first and second commit

---
+++ src/app/girl/girl.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-girl',
+  standalone: true,
+  imports: [],
+  templateUrl: './girl.component.html',
+  styleUrl: './girl.component.css'
+})
+export class GirlComponent {
+
+}
