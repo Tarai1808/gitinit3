4ed77347f4638974f1104591a81fcdbbee83ffcb
Tarai1808 <ashiruzakariyyaaidris6767@gmail.com>
2024-10-07 13:24:35 +0100

    first and second commit

---
+++ src/app/order1/order1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order1',
+  standalone: true,
+  imports: [],
+  templateUrl: './order1.component.html',
+  styleUrl: './order1.component.css'
+})
+export class Order1Component {
+
+}
