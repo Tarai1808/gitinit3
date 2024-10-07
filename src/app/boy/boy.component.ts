89007738702930b72c9ee77016e75c1abe061d0c
Tarai1808 <ashiruzakariyyaaidris6767@gmail.com>
2024-10-07 13:40:47 +0100

    first and second commit

---
+++ src/app/boy/boy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boy',
+  standalone: true,
+  imports: [],
+  templateUrl: './boy.component.html',
+  styleUrl: './boy.component.css'
+})
+export class BoyComponent {
+
+}
